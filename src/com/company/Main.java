2805e0def7231691e14a54de3f529a03d5131cfa
GBTC441017UR <GBTC441017UR@MC.MONTGOMERYCOLLEGE.EDU>
2020-01-31 09:45:34 -0500

    Set up

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+
+        String[] Rice = new String[]{"white", "brown", "none", "all"};
+        String[] Meat = new String[]{"chicken", "steak", "carnidas", "chorizo", "sofritas", "veggies", "none", "all"};
+        String[] Beans = new String[]{"pinto", "black", "none", "all"};
+        String[] Salsa = new String[]{"mild", "medium", "hot", "none", "all"};
+        String[] Veggies = new String[]{"lettuce", "fajita veggies", "none", "all"};
+        boolean Cheese = false;
+        boolean Guac = false;
+        boolean Queso = false;
+        boolean Sourcream = false;
+        double price = 0.0;
+
+    }
+}
